3f32a91398d28de2d978b59c40e8b268ab5a2627
Aaron Faloon <aaron.faloon.2022@uni.strath.ac.uk>
2025-09-29 21:23:47 +0100

    tried again with github pages (again x3)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,34 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { ActivityIndicator, View } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
+
+import { AntDesign, FontAwesome5, MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    ...AntDesign.font,
+    ...FontAwesome5.font,
+    ...MaterialIcons.font,
+    ...MaterialCommunityIcons.font,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return <Stack />;
 }
